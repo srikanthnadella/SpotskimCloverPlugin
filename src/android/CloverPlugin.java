269c51c8f428bf214ce30b5167400d05df284cfd
srikanthnadella <snadella@Srikanths-MacBook-Pro.local>
2014-10-13 20:36:04 -0400

    Made changes to make it exclusively a Clover Plugin
    Removed code for notifications in order to create a new notification
    plugin

--- src/android/CloverPlugin.java
+++ src/android/CloverPlugin.java
@@ -1,29 +1,17 @@
-package com.termtegrity.spotskim.cloverplugin;
- 
+package com.sripra.termtegrity.cloverplugin;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import android.app.Activity;
-import android.content.Intent;
-
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import com.clover.sdk.util.CloverAccount;
 import com.clover.sdk.v1.ResultStatus;
 import com.clover.sdk.v1.ServiceConnector;
@@ -31,44 +19,51 @@ import com.clover.sdk.v1.merchant.Merchant;
 import com.clover.sdk.v1.merchant.MerchantAddress;
 import com.clover.sdk.v1.merchant.MerchantConnector;
 import com.clover.sdk.v1.merchant.MerchantIntent;
-
 import java.text.DateFormat;
 import java.util.Date;
 
 public class CloverPlugin extends CordovaPlugin implements MerchantConnector.OnMerchantChangedListener, ServiceConnector.OnServiceConnectedListener{
     public static final String ACTION_GET_MERCHANT = "getMerchant";
     private static final String TAG = "CloverPlugin";
-
+    private CallbackContext notificationCallbackContext = null;
+    BroadcastReceiver receiver;
+    
     @Override
     public void onMerchantChanged(Merchant merchant) {
         updateMerchant("merchant changed", null, merchant);
     }
-
+    
     @Override
     public void onServiceConnected(ServiceConnector connector) {
         Log.i(TAG, "service connected: " + connector);
     }
-
+    
     @Override
     public void onServiceDisconnected(ServiceConnector connector) {
         Log.i(TAG, "service disconnected: " + connector);
     }
-
+    
+ 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
             if (ACTION_GET_MERCHANT.equals(action)) {
-                //JSONObject arg_object = args.getJSONObject(0);
                 startAccountChooser();
                 connect();
                 getMerchant();
+				JSONObject obj = new JSONObject();
+        try {
+            obj.put("MerchantID", merchantID);
+            obj.put("DeviceID", deviceID);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
+        
+				PluginResult result = new PluginResult(PluginResult.Status.OK, obj);
+            result.setKeepCallback(true);
+            callbackContext.sendPluginResult(result);
 
-                if(merchantConnector == null)
-               callbackContext.error("MerchantConnector is null");
-                else
-                callbackContext.success(merchantID+ "/" +deviceID);
-                disconnect();
-               return true;
+                return true;
             }
             callbackContext.error("Invalid action");
             return false;
@@ -76,49 +71,26 @@ public class CloverPlugin extends CordovaPlugin implements MerchantConnector.OnM
             System.err.println("Exception: " + e.getMessage());
             callbackContext.error("Exception: " + e.getMessage());
             return false;
-        } 
+        }
     }
-
+    
     private static final int REQUEST_ACCOUNT = 0;
     private MerchantConnector merchantConnector;
     private Account account;
     private String merchantID;
     private String deviceID;
-
+    
     private void startAccountChooser() {
-//        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{CloverAccount.CLOVER_ACCOUNT_TYPE}, false, null, null, null, null);
-//        startActivityForResult(intent, REQUEST_ACCOUNT);
         AccountManager accountManager = AccountManager.get(this.cordova.getActivity().getApplicationContext());
         Account[] accounts = accountManager.getAccountsByType(CloverAccount.CLOVER_ACCOUNT_TYPE);
         for (Account acc : accounts){
             account = acc;
             break;
         }
-
+        
     }
-
-//
-//    private void startAccountChooser() {
-//        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{CloverAccount.CLOVER_ACCOUNT_TYPE}, false, null, null, null, null);
-//        startActivityForResult(intent, REQUEST_ACCOUNT);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (requestCode == REQUEST_ACCOUNT) {
-//            if (resultCode == RESULT_OK) {
-//                String name = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-//                String type = data.getStringExtra(AccountManager.KEY_ACCOUNT_TYPE);
-//                account = new Account(name, type);
-//            } else {
-//                if (account == null) {
-//                    finish();
-//                }
-//            }
-//        }
-//    }
-
-
+    
+    
     private void connect() {
         disconnect();
         if (account != null) {
@@ -127,28 +99,28 @@ public class CloverPlugin extends CordovaPlugin implements MerchantConnector.OnM
             merchantConnector.connect();
         }
     }
-
+    
     private void disconnect() {
         if (merchantConnector != null) {
             merchantConnector.disconnect();
             merchantConnector = null;
         }
     }
-
-
+    
+    
     private void getMerchant() {
         Merchant merchant = null;
         try {
             merchant = merchantConnector.getMerchant();
         }
         catch (Exception e){
-
+            
         }
         merchantID = merchant.getId();
         deviceID = merchant.getDeviceId();
-
+        
     }
-
+    
     private void updateMerchant(String status, ResultStatus resultStatus, Merchant result) {
         merchantID = result.getId();
         deviceID = result.getDeviceId();
